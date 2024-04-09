a9953adc8c766000d4aaf8a102f63958bb11f675
Luis Palacios <luisan90@gmail.com>
2024-04-08 18:07:45 -0600

    Fix de roles en AppDbContext

--- TiquiciaRecicla/ProyectoTiquiciaRecicla/Data/AppDbContext.cs
+++ TiquiciaRecicla/ProyectoTiquiciaRecicla/Data/AppDbContext.cs
@@ -32,6 +32,8 @@ namespace ProyectoTiquiciaRecicla.Data
 
         public DbSet<CAT_Centro_Material> CAT_Centros_Materiales { get; set; }
 
+        public DbSet<CAT_Rol> CAT_Roles { get; set; }
+
     }
 
 }
